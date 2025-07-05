68e1fd4fd9044694a98cba5be76fd2754ab9a56e
parushgupta <gparush5@gmail.com>
2025-07-05 20:49:23 +0530

    third commit

--- src/main/java/com/example/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
+++ src/main/java/com/example/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
@@ -1,4 +1,12 @@
 package com.example.springbootwebtutorial.springbootwebtutorial.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmployeeController {
+    @GetMapping(path="/")
+    public String getMesssage(){
+        return "pppppppp";
+    }
 }
--- src/main/java/com/example/springbootwebtutorial/springbootwebtutorial/dto/EmployeeDTO.java
+++ src/main/java/com/example/springbootwebtutorial/springbootwebtutorial/dto/EmployeeDTO.java
@@ -1,4 +1,72 @@
 package com.example.springbootwebtutorial.springbootwebtutorial.dto;
 
+import java.time.LocalDate;
+
 public class EmployeeDTO {
+    Long id;
+    String name;
+    String email;
+    Integer age;
+    Boolean isActive;
+
+    public EmployeeDTO(Long id, String name, String email, Integer age, LocalDate dateOfJoining, Boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dateOfJoining = dateOfJoining;
+        this.isActive = isActive;
+    }
+
+    LocalDate dateOfJoining;
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    public LocalDate getDateOfJoining() {
+        return dateOfJoining;
+    }
+
+    public void setDateOfJoining(LocalDate dateOfJoining) {
+        this.dateOfJoining = dateOfJoining;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
 }
