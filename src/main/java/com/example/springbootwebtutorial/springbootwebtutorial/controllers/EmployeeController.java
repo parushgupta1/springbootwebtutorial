b901ecca5e1ac9c22fbfcc2eb9d7d4ca00017f8a
parushgupta <gparush5@gmail.com>
2025-07-05 21:01:14 +0530

    third commit

--- src/main/java/com/example/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
+++ src/main/java/com/example/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
@@ -1,12 +1,16 @@
 package com.example.springbootwebtutorial.springbootwebtutorial.controllers;
 
+import com.example.springbootwebtutorial.springbootwebtutorial.dto.EmployeeDTO;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 public class EmployeeController {
-    @GetMapping(path="/")
-    public String getMesssage(){
-        return "pppppppp";
+    @GetMapping(path="/employees/{employeeId}")
+    public EmployeeDTO getEmployeeById(@PathVariable String employeeId){
+  return  new EmployeeDTO(1L,"parus","parush@parush.com",27, LocalDate.now(),Boolean.TRUE);
     }
 }
